7960fc1390661605f3fe7e96c99a74dfba4586ed
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-01-25 00:57:44 +0900

    [end] ToDoList 완성

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "658a5e60c8b281dd4a13c609ea4c835b";
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live it", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find  you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
