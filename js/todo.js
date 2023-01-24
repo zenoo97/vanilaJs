34005817027327be30856374e29211f55d9c4697
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-01-24 16:09:45 +0900

    [Update] todo list 추가

--- js/todo.js
+++ js/todo.js
@@ -1,8 +1,22 @@
 const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+  console.log(toDoList);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
-console.log("asd");
+
+// 7번 째 라인에서 input의 현재 value를 새로운 변수에 복사하는 것
