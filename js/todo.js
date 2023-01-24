084a54ccba27dfd2d5b44b52ada787a750a99ebb
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-01-24 16:37:38 +0900

    [Update] delete 추가

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,21 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
-  console.log(toDoList);
 }
 
 function handleToDoSubmit(event) {
@@ -18,5 +26,4 @@ function handleToDoSubmit(event) {
   paintToDo(newTodo);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
 // 7번 째 라인에서 input의 현재 value를 새로운 변수에 복사하는 것
